c978c10770ace97970c112974814dbd6292c612e
Peter <peter@addressforall.org>
2025-03-22 18:39:53 -0300

    rm colombia

--- src/step01-extraSRID.sql
+++ src/step01-extraSRID.sql
@@ -30,36 +30,7 @@ INSERT INTO spatial_ref_sys (srid, auth_name, auth_srid, proj4text, srtext) VALU
   UNIT["Meter",1]
  ]$$
 ),
-  -- -- -- --
-( -- Grid of Colombia, IGAC MAGNA-SIRGAS / Origen-Nacional:
-  9377, -- official EPSG number
-  'CO:IGAC',
-  9377,
-  '+proj=tmerc +lat_0=4.0 +lon_0=-73.0 +k=0.9992 +x_0=5000000 +y_0=2000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs',
-  $$PROJCS[
-    "MAGNA-SIRGAS / Origen-Nacional",
-    GEOGCS[
-      "MAGNA-SIRGAS",
-      DATUM[
-        "Marco_Geocentrico_Nacional_de_Referencia",
-        SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],
-        TOWGS84[0,0,0,0,0,0,0],
-        AUTHORITY["EPSG","6686"]
-      ],
-      PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],
-      UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],
-      AUTHORITY["EPSG","4686"]
-    ],
-    PROJECTION["Transverse_Mercator"],
-    PARAMETER["latitude_of_origin",4.0],
-    PARAMETER["central_meridian",-73.0],
-    PARAMETER["scale_factor",0.9992],
-    PARAMETER["false_easting",5000000],
-    PARAMETER["false_northing",2000000],
-    UNIT["metre",1,AUTHORITY["EPSG","9001"]],
-    AUTHORITY["EPSG","9377"]
-  ]$$
-),
+
   -- -- -- --
   --  "DGGS projections" from SRID 955001 to 955099.
   --
