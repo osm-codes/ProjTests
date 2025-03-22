17f767d55d613fb5d2bae6302dedfd9ae9f7e407
Peter <peter@addressforall.org>
2025-03-22 10:08:49 -0300

    no functional changes
    Only using variable names and formating spaces on source-code.

--- src/area_correctByElev.sql
+++ src/area_correctByElev.sql
@@ -14,50 +14,54 @@ CREATE FUNCTION projtest.geodistance(
   earthdia float default 12756*1000.0 -- meters
 ) RETURNS float AS
 $f$
-SELECT asin(
-  sqrt(
-    sin(radians($3-$1)/2)^2 +
-    sin(radians($4-$2)/2)^2 *
-    cos(radians($1)) *
-    cos(radians($3))
-  )
-) * ($5/1000.0) AS distance;
--- earthdia use 2 * 6378  km = 12756 km
+ SELECT asin(
+   sqrt(
+     sin(radians(blat-alat)/2)^2 +
+     sin(radians(blng-alng)/2)^2 *
+     cos(radians(alat)) *
+     cos(radians(blat))
+   )
+ ) * ($5/1000.0) AS distance;
+ -- earthdia use 2 * 6378  km = 12756 km
 $f$ language SQL IMMUTABLE
 ;
 
 CREATE FUNCTION projtest.area_correction_factor(
-  probe_geom geometry,  -- any geometry
-  alt float       -- local elevation in m
+  probe_geom geometry,  -- any geometry, as "test probe" (sonde)
+  alt        float      -- local elevation (altitude) in meters
 ) RETURNS float AS $f$
-WITH points AS (
+WITH probe_points AS (
   select 
         st_pointn(st_exteriorring($1),1) as p1, 
-        st_pointn(st_exteriorring($1),
-        (st_numpoints(st_exteriorring($1))/2)::int) as p2
+        st_pointn(
+          st_exteriorring($1),
+          (st_numpoints(st_exteriorring($1))/2)::int
+        ) as p2
 ),
 earth_rad AS ( 
   select sqrt(
-            (((6378137.0^2) * cos(st_y(st_pointn(st_exteriorring($1),1))))^2 + ((6356752.3142^2) * sin(st_y(st_pointn(st_exteriorring($1),1))))^2)/
-                ((6378137.0 * cos(st_y(st_pointn(st_exteriorring($1),1))))^2 + (6356752.3142 * sin(st_y(st_pointn(st_exteriorring($1),1))))^2)
-            ) as erad
+            (((6378137.0^2) * cos(st_y(p1)))^2 + ((6356752.3142^2) * sin(st_y(p1)))^2)
+            /
+            ((6378137.0 * cos(st_y(p1)))^2 + (6356752.3142 * sin(st_y(p1)))^2)
+         ) as erad
+  from probe_points
   )
 SELECT ---- the CORRECTION FACTOR: ----
     projtest.geodistance(
-      st_y((select p1 from points)),
-      st_x((select p1 from points)),
-      st_y((select p2 from points)),
-      st_x((select p2 from points)),
+      st_y((select p1 from probe_points)),
+      st_x((select p1 from probe_points)),
+      st_y((select p2 from probe_points)),
+      st_x((select p2 from probe_points)),
       (SELECT erad FROM earth_rad) + $2
     )
     /
-    projtest.geodistance(
-      st_y((select p1 from points)), 
-      st_x((select p1 from points)),
-      st_y((select p2 from points)),
-      st_x((select p2 from points)), 
+    projtest.geodistance( 
+      st_y((select p1 from probe_points)), 
+      st_x((select p1 from probe_points)),
+      st_y((select p2 from probe_points)),
+      st_x((select p2 from probe_points)), 
       (SELECT erad FROM earth_rad)
-    )
+    ) -- same as ST_Distance(p1,p2,true)?
 $f$ language SQL immutable;
 
 CREATE VIEW projtest.vw01_area_factors AS
