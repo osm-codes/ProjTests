84f382244640893f5a51140e7a7d621d7d53d287
Peter Krauss <ppkrauss@gmail.com>
2025-03-31 08:18:44 -0300

    add elevation summary

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ProjTests
-Projection tests of PROJ.org projections, using PostGIS
+Map projection tests of [PROJ.org library](https://PROJ.org)  projections, using PostGIS.
 
 ## Install
 On Linux with PostgreSQL v9+.
@@ -7,6 +7,36 @@ On Linux with PostgreSQL v9+.
 cd ProjTests/src
 cat step0*.sql | psql postgres://postgres@localhost/test
 ```
+## Testing DGGS
+Some [DGGS projections](https://www.iso.org/standard/32588.html) with "good fit to WGS84" claims ([ref1](https://doi.org/10.1080/20964471.2022.2094926), [ref2](https://doi.org/10.1080/17538947.2019.1645893)), that are not seems realistic for Brazilian territory.
+
+At [this PostGIS SQL scripts](src), using `ST_Area(geom,true)` as reference of "WGS84 area measure", and `ST_Area(geom)` as projected sample, we can reproduce the `projtest.vw06_proj_cmp_summary` report:
+
+projection label and definition            | proj_diff_avg 
+--------------------------------|---------------
+_BR:IBGE_  &nbsp; [classical Albers equal-area](https://proj.org/en/stable/operations/projections/aea.html),<br> `+proj=aea +lat_0=-12 +lon_0=-54 +lat_1=-2 +lat_2=-22 +ellps=WGS84`          | 0.0%
+*DGGS:001:rHEALPix*  &nbsp; [`+proj=rhealpix`](https://proj.org/en/stable/operations/projections/rhealpix.html) (default `+ellps=GRS80`) | 17.8%
+*DGGS:002:rHEALPix*   &nbsp; `+proj=rhealpix +ellps=WGS84` | **17.8%**
+ *DGGS:030:ISEA*   &nbsp;  [`+proj=isea`](https://proj.org/en/stable/operations/projections/isea.html)   | 0.5%
+ *DGGS:032:ISEA*    &nbsp; `+proj=isea +ellps=WGS84`  | 0.5%
+
+The column `proj_diff_avg` is the average of area-difference of some sample zones (Geohash rectangles with different areas) at Brazil. The VIEW `projtest.vw02_proj_cmp_raw` shows raw data. To see Geohash cells ("ghs" column), use [viewer1](https://geohash.softeng.co/6dxqw4) or [viewer2](https://www.movable-type.co.uk/scripts/geohash.html).
+
+The projection _BR:IBGE_ is the official, used by Census, demonstrating that PROJ Lib can fit to WGS84.
+
+## Testing area distortion by elevation
+Using `projtest.vw04_area_factors_summary` we can show that the "WGS84 area measure" distortion (column "km2_change") grows with elevation, and that from \~1 km elevation upwards, the distortion of \~0.01% (or more) cannot be ignored.
+
+city_name         | elevation | km2_change 
+-------------------------|----------|------------
+Fortaleza                |        20 | 0.0003%
+ Uruguaiana               |        70 | 0.0011%
+ Manaus                   |        90 | 0.0014%
+ Santa Maria              |       110 | 0.0017%
+ Trindade (ilha)          |       170 | 0.0027%
+ Sao Paulo                |       760 | 0.0120%
+ Mané Garrincha (estadio) |      1200 | 0.0188%
+ La Paz                   |      3650 | 0.0573%
 
 ## License
 [CC0](https://creativecommons.org/publicdomain/zero/1.0/).
--- src/step03-reports.sql
+++ src/step03-reports.sql
@@ -14,17 +14,17 @@ CREATE VIEW projtest.vw01_SRIDs AS
 ;
 CREATE VIEW projtest.vw03_refPoints AS
   SELECT * FROM ( VALUES
-    (ST_Point(-46.633956,-23.550385,4326), 'BR', 'Sao Paulo', 760 ), 
+    (ST_Point(-46.633956,-23.550385,4326), 'BR', 'Sao Paulo', 760 ),
     (ST_Point(-68.13333,-16.496,4326), 'BO', 'La Paz', 3650 ),
-    (ST_Point(-60.023333,-3.130278,4326), 'BR', 'Manaus', 90 ), 
-    (ST_Point(-38.522481,-3.807267,4326), 'BR', 'Fortaleza', 20 ), 
-    (ST_Point(-57.03694, -29.78333,4326), 'BR', 'Uruguaiana', 70 ), 
+    (ST_Point(-60.023333,-3.130278,4326), 'BR', 'Manaus', 90 ),
+    (ST_Point(-38.522481,-3.807267,4326), 'BR', 'Fortaleza', 20 ),
+    (ST_Point(-57.03694, -29.78333,4326), 'BR', 'Uruguaiana', 70 ),
     (ST_Point(-53.807348,-29.685718,4326), 'BR', 'Santa Maria', 110 ),
-    (ST_Point(-47.899213,-15.78340,4326), 'BR', 'Mané Garrincha (estadio)', 1200 ), 
+    (ST_Point(-47.899213,-15.78340,4326), 'BR', 'Mané Garrincha (estadio)', 1200 ),
     (ST_Point(-29.326123,-20.508324,4326), 'BR', 'Trindade (ilha)', 170 )
   ) t1 (pt_geom,country_code,city_name,elevation)
 ;
-       
+
 CREATE VIEW projtest.vw05_samples AS
    SELECT *, ST_SetSRID(ST_GeomFromGeoHash(ghs),4326) geom
    FROM generate_series(5,7) t0(ghs_digits), LATERAL (
@@ -45,7 +45,7 @@ CREATE VIEW projtest.vw02_proj_cmp_raw AS
   round( ST_Area(geom)/1000.0^2, 5) as proj_km2
  FROM (
   SELECT i.*, s.ghs, s.city_name, s.elevation,
-         ST_Area(s.geom,true) AS m2, 
+         ST_Area(s.geom,true) AS m2,
          ST_Transform(s.geom, i.srid) as geom
   FROM projtest.vw01_SRIDs i, projtest.vw05_samples s
   WHERE s.country_code='BR'
@@ -54,7 +54,7 @@ CREATE VIEW projtest.vw02_proj_cmp_raw AS
 ;
 
 CREATE VIEW projtest.vw04_proj_cmp_full AS
- SELECT proj_name, 
+ SELECT proj_name,
     ghs ||' ('|| city_name ||')' as ghs_and_city,
     km2,
     proj_km2,
@@ -76,14 +76,30 @@ CREATE VIEW projtest.vw04_area_factors AS
   round( sea_m2*afat/1000^2, 5) as elev_km2,
   round(100.0*afat - 100, 4)::text||'%' km2_change
  FROM (
-  SELECT *, ST_Area(geom,true) AS sea_m2, 
+  SELECT *, ST_Area(geom,true) AS sea_m2,
             projtest.area_correction_factor(geom,elevation) as afat
   FROM projtest.vw05_samples
  ) t1 ORDER BY 1,2
 ;
 
+
+CREATE VIEW projtest.vw04_area_factors_summary AS
+ -- adapted from https://gis.stackexchange.com/q/491266/7505
+ SELECT city_name, elevation,
+  round(100.0*avg(afat) - 100, 4)::text||'%' km2_change
+ FROM (
+  SELECT *, ST_Area(geom,true) AS sea_m2,
+            projtest.area_correction_factor(geom,elevation) as afat
+  FROM projtest.vw05_samples
+ ) t1
+ GROUP BY 1,2
+ ORDER BY 2
+;
+
+
 /* ----- SHOW report:
 SELECT * FROM projtest.vw04_proj_cmp_full ;
 SELECT * FROM projtest.vw06_proj_cmp_summary;
 SELECT * FROM projtest.vw04_area_factors;
+SELECT * FROM projtest.vw04_area_factors_summary;
 */
